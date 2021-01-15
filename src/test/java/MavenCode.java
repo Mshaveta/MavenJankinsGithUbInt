9a278305eaa8c1d9f6c34295c66166aa365f05a3
mshaveta <shaveta.malhotra47@gmail.com>
2021-01-16 00:36:05 +0530

    Done

---
+++ src/test/java/MavenCode.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MavenCode {
+	WebDriver driver;
+	@Test(priority=1)
+	public void login() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+	}
+}
